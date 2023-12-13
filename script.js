5c1f9fbf6342e21b6cbfffd305095da433832e47
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-12-13 16:21:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+// script.js
 function toCase(text) {
-  // write your code here
+    if (text === "") {
+        return "-";
+    }
+
+    const lowercase = text.toLowerCase();
+    const uppercase = text.toUpperCase();
+    const result = `${lowercase}-${uppercase}`;
+
+    return result;
 }
 
 // DO not change the code below
 
-const text = prompt("Enter text:");
-alert(toCase(text));
+const inputText = prompt("Enter text:");
+alert(toCase(inputText));
